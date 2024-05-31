4eadda50ed118b35ecc2804d176ed1c09d504404
Matthew McDermott <mattmcdermott8@gmail.com>
2024-05-31 18:49:12 -0400

    Not remotely working; moving to local for dev

---
+++ eICU_Example/joint_script.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+MIMICIV_RAW_DIR="$1"
+MIMICIV_PREMEDS_DIR="$2"
+MIMICIV_MEDS_DIR="$3"
+N_PARALLEL_WORKERS="$4"
+
+shift 4
+
+echo "Running pre-MEDS conversion."
+./MIMIC-IV_Example/pre_MEDS.py raw_cohort_dir=$MIMICIV_RAW_DIR output_dir=$MIMICIV_PREMEDS_DIR
+
+echo "Running shard_events.py with $N_PARALLEL_WORKERS workers in parallel"
+./scripts/extraction/shard_events.py \
+    --multirun \
+    worker="range(0,$N_PARALLEL_WORKERS)" \
+    hydra/launcher=joblib \
+    input_dir=$MIMICIV_PREMEDS_DIR \
+    cohort_dir=$MIMICIV_MEDS_DIR \
+    event_conversion_config_fp=./MIMIC-IV_Example/configs/event_configs.yaml "$@"
+
+echo "Splitting patients in serial"
+./scripts/extraction/split_and_shard_patients.py \
+    input_dir=$MIMICIV_PREMEDS_DIR \
+    cohort_dir=$MIMICIV_MEDS_DIR \
+    event_conversion_config_fp=./MIMIC-IV_Example/configs/event_configs.yaml "$@"
+
+echo "Converting to sharded events with $N_PARALLEL_WORKERS workers in parallel"
+./scripts/extraction/convert_to_sharded_events.py \
+    --multirun \
+    worker="range(0,$N_PARALLEL_WORKERS)" \
+    hydra/launcher=joblib \
+    input_dir=$MIMICIV_PREMEDS_DIR \
+    cohort_dir=$MIMICIV_MEDS_DIR \
+    event_conversion_config_fp=./MIMIC-IV_Example/configs/event_configs.yaml "$@"
+
+echo "Merging to a MEDS cohort with $N_PARALLEL_WORKERS workers in parallel"
+./scripts/extraction/merge_to_MEDS_cohort.py \
+    --multirun \
+    worker="range(0,$N_PARALLEL_WORKERS)" \
+    hydra/launcher=joblib \
+    input_dir=$MIMICIV_PREMEDS_DIR \
+    cohort_dir=$MIMICIV_MEDS_DIR \
+    event_conversion_config_fp=./MIMIC-IV_Example/configs/event_configs.yaml "$@"
