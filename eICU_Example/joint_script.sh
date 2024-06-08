f48ddb72a645c0b9cec2fc107a911be801373c97
Matthew McDermott <mattmcdermott8@gmail.com>
2024-06-08 16:46:00 -0400

    Added a note to eICU example

--- eICU_Example/joint_script.sh
+++ eICU_Example/joint_script.sh
@@ -39,32 +39,37 @@ N_PARALLEL_WORKERS="$4"
 
 shift 4
 
-echo "Running pre-MEDS conversion."
-./eICU_Example/pre_MEDS.py raw_cohort_dir="$EICU_RAW_DIR" output_dir="$EICU_PREMEDS_DIR"
+echo "Note that eICU is expensive (in memory) in some final stages as each MEDS shards will end up being "
+echo "large in # of rows (e.g., ~175M) given the frequency of periodic vitals signs. We recommend setting "
+echo "stage_configs.merge_to_MEDS_cohort.unique_by=null in order to mitigate the cost of the unique "
+echo "operation at to avoid OOM issues."
 
-echo "Running shard_events.py with $N_PARALLEL_WORKERS workers in parallel"
-./scripts/extraction/shard_events.py \
-    --multirun \
-    worker="range(0,$N_PARALLEL_WORKERS)" \
-    hydra/launcher=joblib \
-    input_dir="$EICU_PREMEDS_DIR" \
-    cohort_dir="$EICU_MEDS_DIR" \
-    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
-
-echo "Splitting patients in serial"
-./scripts/extraction/split_and_shard_patients.py \
-    input_dir="$EICU_PREMEDS_DIR" \
-    cohort_dir="$EICU_MEDS_DIR" \
-    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
-
-echo "Converting to sharded events with $N_PARALLEL_WORKERS workers in parallel"
-./scripts/extraction/convert_to_sharded_events.py \
-    --multirun \
-    worker="range(0,$N_PARALLEL_WORKERS)" \
-    hydra/launcher=joblib \
-    input_dir="$EICU_PREMEDS_DIR" \
-    cohort_dir="$EICU_MEDS_DIR" \
-    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
+#echo "Running pre-MEDS conversion."
+#./eICU_Example/pre_MEDS.py raw_cohort_dir="$EICU_RAW_DIR" output_dir="$EICU_PREMEDS_DIR"
+#
+#echo "Running shard_events.py with $N_PARALLEL_WORKERS workers in parallel"
+#./scripts/extraction/shard_events.py \
+#    --multirun \
+#    worker="range(0,$N_PARALLEL_WORKERS)" \
+#    hydra/launcher=joblib \
+#    input_dir="$EICU_PREMEDS_DIR" \
+#    cohort_dir="$EICU_MEDS_DIR" \
+#    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
+#
+#echo "Splitting patients in serial"
+#./scripts/extraction/split_and_shard_patients.py \
+#    input_dir="$EICU_PREMEDS_DIR" \
+#    cohort_dir="$EICU_MEDS_DIR" \
+#    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
+#
+#echo "Converting to sharded events with $N_PARALLEL_WORKERS workers in parallel"
+#./scripts/extraction/convert_to_sharded_events.py \
+#    --multirun \
+#    worker="range(0,$N_PARALLEL_WORKERS)" \
+#    hydra/launcher=joblib \
+#    input_dir="$EICU_PREMEDS_DIR" \
+#    cohort_dir="$EICU_MEDS_DIR" \
+#    event_conversion_config_fp=./eICU_Example/configs/event_configs.yaml "$@"
 
 echo "Merging to a MEDS cohort with $N_PARALLEL_WORKERS workers in parallel"
 ./scripts/extraction/merge_to_MEDS_cohort.py \
