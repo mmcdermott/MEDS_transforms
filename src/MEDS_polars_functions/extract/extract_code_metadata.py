99f3d5d8786f126150a0c3c7e4a5a4044bf05a27
Matthew McDermott <mattmcdermott8@gmail.com>
2024-07-19 15:32:46 -0400

    More starter code -- not working or tested or even implemented yet.

--- src/MEDS_polars_functions/extract/extract_code_metadata.py
+++ src/MEDS_polars_functions/extract/extract_code_metadata.py
@@ -3,7 +3,6 @@
 
 import copy
 import random
-from collections.abc import Sequence
 from functools import partial, reduce
 from pathlib import Path
 
@@ -14,6 +13,7 @@ from omegaconf import DictConfig, OmegaConf
 from omegaconf.listconfig import ListConfig
 
 from MEDS_polars_functions.extract import CONFIG_YAML
+from MEDS_polars_functions.extract.utils import get_supported_fp
 from MEDS_polars_functions.mapreduce.mapper import rwlock_wrap
 from MEDS_polars_functions.utils import (
     is_col_field,
@@ -40,9 +40,8 @@ def extract_metadata(df: pl.LazyFrame, event_cfg: dict[str, str | None]) -> pl.L
     Returns:
         A DataFrame containing the metadata extracted and linked to appropriately constructed code strings for
         the event configuration. The output DataFrame will contain at least two columns: `"code"` and whatever
-        metadata column is specified for extraction in the metadata block. The output dataframe will be unique
-        by code. Multiple metadata entries for the same code may be collapsed into a list column field in the
-        output if the input metadata is not unique by code.
+        metadata column is specified for extraction in the metadata block. The output dataframe will not
+        necessarily be unique by code if the input metadata is not unique by code.
 
     Raises:
         KeyError: If the event configuration dictionary is missing the `"code"` or `"_metadata"` keys or if
@@ -84,23 +83,30 @@ def extract_metadata(df: pl.LazyFrame, event_cfg: dict[str, str | None]) -> pl.L
         ...     "code": ["A", "A", "C", "D"],
         ...     "code_modifier": ["1", "1", "2", "3"],
         ...     "code_modifier_2": ["1", "2", "3", "4"],
-        ...     "name": ["A-1-1", "A-1-2", "C-2-3", "D-3-4"],
+        ...     "title": ["A-1-1", "A-1-2", "C-2-3", "D-3-4"],
         ... })
         >>> event_cfg = {
         ...     "code": ["FOO", "col(code)", "col(code_modifier)"],
-        ...     "_metadata": {"desc": "name"},
+        ...     "_metadata": {
+        ...         "desc": "title",
+        ...         "parent_code": {
+        ...             "OUT_VAL_1": {"code_modifier_2": 2},
+        ...             "OUT_VAL_2": {"code_modifier_2": 3},
+        ...         },
+        ...     },
         ... }
         >>> extract_metadata(raw_metadata, event_cfg)
         shape: (4, 2)
-        ┌───────────┬────────────────────┐
-        │ code      ┆ desc               │
-        │ ---       ┆ ---                │
-        │ cat       ┆ list[str]          │
-        ╞═══════════╪════════════════════╡
-        │ FOO//A//1 ┆ [A-1-1,A-1-2]      │
-        │ FOO//C//2 ┆ [C-2-3]            │
-        │ FOO//D//3 ┆ [D-3-4]            │
-        └───────────┴────────────────────┘
+        ┌───────────┬───────┬───────────────┐
+        │ code      ┆ desc  ┆ parent_code   │
+        │ ---       ┆ ---   ┆ ---           │
+        │ cat       ┆ str   ┆ str           │
+        ╞═══════════╪═══════╪═══════════════╡
+        │ FOO//A//1 ┆ A-1-1 ┆               │
+        │ FOO//A//1 ┆ A-1-2 ┆ OUT_VAL_1     │
+        │ FOO//C//2 ┆ C-2-3 ┆ OUT_VAL_2     │
+        │ FOO//D//3 ┆ D-3-4 ┆               │
+        └───────────┴───────┴───────────────┘
     """  # noqa: E501
     df = df
     if "code" not in event_cfg:
@@ -183,198 +189,16 @@ def extract_metadata(df: pl.LazyFrame, event_cfg: dict[str, str | None]) -> pl.L
     return df
 
 
-def convert_to_events(
-    df: pl.LazyFrame, event_cfgs: dict[str, dict[str, str | None | Sequence[str]]]
-) -> pl.LazyFrame:
-    """Converts a DataFrame of raw data into a DataFrame of events.
-
-    Args:
-        df: The raw data DataFrame. This must have a `"patient_id"` column containing the patient ID. The
-            other columns it must have are determined by the `event_cfgs` configuration dictionary.
-            For the precise mechanism of column determination, see the `extract_event` function.
-        event_cfgs: A dictionary containing the configurations for the events to extract. The keys of this
-            dictionary should be the names of the events to extract (these are only used for logging, and will
-            not automatically appear in any manner in the output data), and the values should be dictionaries
-            containing the configuration for each event. Each event configuration dictionary should have the
-            same structure as the `event_cfg` dictionary in the `extract_event` function. Please see that
-            function for further details on how these configuration dictionaries should be structured and are
-            used to extract events.
-
-    Returns:
-        A DataFrame containing the events extracted from the raw data. This DataFrame will contain all the
-        events extracted from the raw data, with the rows from each event DataFrame concatenated together.
-        After concatenation, this dataframe will not be deduplicated, so if the raw data results in duplicates
-        across events of different name, these will be preserved in the output DataFrame.
-        The output DataFrame will contain at least three columns: `"patient_id"`, `"code"`, and `"timestamp"`.
-        If any events have additional columns, these will be included in the output DataFrame as well. All
-        columns across all event configurations will be included in the output DataFrame, with `null` values
-        filled in for events that do not have a particular column.
-
-    Raises:
-        ValueError: If no event configurations are provided or if an error occurs during event extraction.
-
-    Examples:
-        >>> _ = pl.Config.set_tbl_width_chars(600)
-        >>> _ = pl.Config.set_tbl_rows(20)
-        >>> _ = pl.Config.set_tbl_cols(20)
-        >>> from datetime import datetime
-        >>> complex_raw_data = pl.DataFrame(
-        ...     {
-        ...         "patient_id": [1, 1, 2, 2, 2, 3],
-        ...         "admission_time": [
-        ...             "2021-01-01 00:00:00",
-        ...             "2021-01-02 00:00:00",
-        ...             "2021-01-03 00:00:00",
-        ...             "2021-01-04 00:00:00",
-        ...             "2021-01-05 00:00:00",
-        ...             "2021-01-06 00:00:00",
-        ...         ],
-        ...         "discharge_time": [
-        ...             datetime(2021, 1, 1, 11, 23, 45),
-        ...             datetime(2021, 1, 2, 12, 34, 56),
-        ...             datetime(2021, 1, 3, 13, 45, 56),
-        ...             datetime(2021, 1, 4, 14, 56, 45),
-        ...             datetime(2021, 1, 5, 15, 23, 45),
-        ...             datetime(2021, 1, 6, 16, 34, 56),
-        ...         ],
-        ...         "admission_type": ["A", "B", "C", "D", "E", "F"],
-        ...         "discharge_location": ["Home", "SNF", "Home", "SNF", "Home", "SNF"],
-        ...         "severity_score": [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
-        ...         "death_time": [
-        ...             "2023/01/01",
-        ...             "2023/01/01",
-        ...             "2023/01/04",
-        ...             "2023/01/04",
-        ...             "2023/01/04",
-        ...             "2023/01/07"
-        ...         ],
-        ...         "eye_color": ["blue", "blue", "green", "green", "green", "brown"],
-        ...     },
-        ...     schema={
-        ...         "patient_id": pl.UInt8,
-        ...         "admission_time": pl.Utf8,
-        ...         "discharge_time": pl.Datetime,
-        ...         "admission_type": pl.Utf8,
-        ...         "discharge_location": pl.Categorical,
-        ...         "severity_score": pl.Float64,
-        ...         "death_time": pl.Utf8,
-        ...         "eye_color": pl.Categorical,
-        ...     },
-        ... )
-        >>> event_cfgs = {
-        ...     "admission": {
-        ...         "code": "ADMISSION",
-        ...         "timestamp": "col(admission_time)",
-        ...         "timestamp_format": "%Y-%m-%d %H:%M:%S",
-        ...         "admission_type": "admission_type",
-        ...         "severity_on_admission": "severity_score",
-        ...     },
-        ...     "discharge": {
-        ...         "code": "DISCHARGE",
-        ...         "timestamp": "col(discharge_time)",
-        ...         "discharge_location": "discharge_location",
-        ...     },
-        ...     "death": {
-        ...         "code": "DEATH",
-        ...         "timestamp": "col(death_time)",
-        ...         "timestamp_format": "%Y/%m/%d",
-        ...     },
-        ...     "eye_color": {
-        ...         "code": "EYE_COLOR",
-        ...         "timestamp": None,
-        ...         "eye_color": "eye_color",
-        ...     },
-        ... }
-        >>> # We'll print the raw data so you can see what it looks like
-        >>> complex_raw_data
-        shape: (6, 8)
-        ┌────────────┬─────────────────────┬─────────────────────┬────────────────┬────────────────────┬────────────────┬────────────┬───────────┐
-        │ patient_id ┆ admission_time      ┆ discharge_time      ┆ admission_type ┆ discharge_location ┆ severity_score ┆ death_time ┆ eye_color │
-        │ ---        ┆ ---                 ┆ ---                 ┆ ---            ┆ ---                ┆ ---            ┆ ---        ┆ ---       │
-        │ u8         ┆ str                 ┆ datetime[μs]        ┆ str            ┆ cat                ┆ f64            ┆ str        ┆ cat       │
-        ╞════════════╪═════════════════════╪═════════════════════╪════════════════╪════════════════════╪════════════════╪════════════╪═══════════╡
-        │ 1          ┆ 2021-01-01 00:00:00 ┆ 2021-01-01 11:23:45 ┆ A              ┆ Home               ┆ 1.0            ┆ 2023/01/01 ┆ blue      │
-        │ 1          ┆ 2021-01-02 00:00:00 ┆ 2021-01-02 12:34:56 ┆ B              ┆ SNF                ┆ 2.0            ┆ 2023/01/01 ┆ blue      │
-        │ 2          ┆ 2021-01-03 00:00:00 ┆ 2021-01-03 13:45:56 ┆ C              ┆ Home               ┆ 3.0            ┆ 2023/01/04 ┆ green     │
-        │ 2          ┆ 2021-01-04 00:00:00 ┆ 2021-01-04 14:56:45 ┆ D              ┆ SNF                ┆ 4.0            ┆ 2023/01/04 ┆ green     │
-        │ 2          ┆ 2021-01-05 00:00:00 ┆ 2021-01-05 15:23:45 ┆ E              ┆ Home               ┆ 5.0            ┆ 2023/01/04 ┆ green     │
-        │ 3          ┆ 2021-01-06 00:00:00 ┆ 2021-01-06 16:34:56 ┆ F              ┆ SNF                ┆ 6.0            ┆ 2023/01/07 ┆ brown     │
-        └────────────┴─────────────────────┴─────────────────────┴────────────────┴────────────────────┴────────────────┴────────────┴───────────┘
-        >>> convert_to_events(complex_raw_data, event_cfgs)
-        shape: (18, 7)
-        ┌────────────┬───────────┬─────────────────────┬────────────────┬───────────────────────┬────────────────────┬───────────┐
-        │ patient_id ┆ code      ┆ timestamp           ┆ admission_type ┆ severity_on_admission ┆ discharge_location ┆ eye_color │
-        │ ---        ┆ ---       ┆ ---                 ┆ ---            ┆ ---                   ┆ ---                ┆ ---       │
-        │ u8         ┆ cat       ┆ datetime[μs]        ┆ cat            ┆ f64                   ┆ cat                ┆ cat       │
-        ╞════════════╪═══════════╪═════════════════════╪════════════════╪═══════════════════════╪════════════════════╪═══════════╡
-        │ 1          ┆ ADMISSION ┆ 2021-01-01 00:00:00 ┆ A              ┆ 1.0                   ┆ null               ┆ null      │
-        │ 1          ┆ ADMISSION ┆ 2021-01-02 00:00:00 ┆ B              ┆ 2.0                   ┆ null               ┆ null      │
-        │ 2          ┆ ADMISSION ┆ 2021-01-03 00:00:00 ┆ C              ┆ 3.0                   ┆ null               ┆ null      │
-        │ 2          ┆ ADMISSION ┆ 2021-01-04 00:00:00 ┆ D              ┆ 4.0                   ┆ null               ┆ null      │
-        │ 2          ┆ ADMISSION ┆ 2021-01-05 00:00:00 ┆ E              ┆ 5.0                   ┆ null               ┆ null      │
-        │ 3          ┆ ADMISSION ┆ 2021-01-06 00:00:00 ┆ F              ┆ 6.0                   ┆ null               ┆ null      │
-        │ 1          ┆ DISCHARGE ┆ 2021-01-01 11:23:45 ┆ null           ┆ null                  ┆ Home               ┆ null      │
-        │ 1          ┆ DISCHARGE ┆ 2021-01-02 12:34:56 ┆ null           ┆ null                  ┆ SNF                ┆ null      │
-        │ 2          ┆ DISCHARGE ┆ 2021-01-03 13:45:56 ┆ null           ┆ null                  ┆ Home               ┆ null      │
-        │ 2          ┆ DISCHARGE ┆ 2021-01-04 14:56:45 ┆ null           ┆ null                  ┆ SNF                ┆ null      │
-        │ 2          ┆ DISCHARGE ┆ 2021-01-05 15:23:45 ┆ null           ┆ null                  ┆ Home               ┆ null      │
-        │ 3          ┆ DISCHARGE ┆ 2021-01-06 16:34:56 ┆ null           ┆ null                  ┆ SNF                ┆ null      │
-        │ 1          ┆ DEATH     ┆ 2023-01-01 00:00:00 ┆ null           ┆ null                  ┆ null               ┆ null      │
-        │ 2          ┆ DEATH     ┆ 2023-01-04 00:00:00 ┆ null           ┆ null                  ┆ null               ┆ null      │
-        │ 3          ┆ DEATH     ┆ 2023-01-07 00:00:00 ┆ null           ┆ null                  ┆ null               ┆ null      │
-        │ 1          ┆ EYE_COLOR ┆ null                ┆ null           ┆ null                  ┆ null               ┆ blue      │
-        │ 2          ┆ EYE_COLOR ┆ null                ┆ null           ┆ null                  ┆ null               ┆ green     │
-        │ 3          ┆ EYE_COLOR ┆ null                ┆ null           ┆ null                  ┆ null               ┆ brown     │
-        └────────────┴───────────┴─────────────────────┴────────────────┴───────────────────────┴────────────────────┴───────────┘
-        >>> convert_to_events(complex_raw_data, {})
-        Traceback (most recent call last):
-            ...
-        ValueError: No event configurations provided.
-        >>> convert_to_events(complex_raw_data, {"admission": {}})
-        Traceback (most recent call last):
-            ...
-        ValueError: Error extracting event admission: ...
-    """  # noqa: E501
-
-    if not event_cfgs:
-        raise ValueError("No event configurations provided.")
-
-    event_dfs = []
-    for event_name, event_cfg in event_cfgs.items():
-        try:
-            logger.info(f"Building computational graph for extracting {event_name}")
-            event_dfs.append(extract_event(df, event_cfg))
-        except Exception as e:
-            raise ValueError(f"Error extracting event {event_name}: {e}") from e
-
-    df = pl.concat(event_dfs, how="diagonal_relaxed")
-    return df
-
-
 def get_events_and_metadata_by_metadata_fp(event_configs):
-    raise NotImplementedError("This function is not yet implemented.")
+    """This reformats the event conversion config to map metadata file input prefixes to linked event
+    configs."""
 
-
-def find_metadata_fp(metadata_input_dir, input_prefix):
     raise NotImplementedError("This function is not yet implemented.")
 
 
 @hydra.main(version_base=None, config_path=str(CONFIG_YAML.parent), config_name=CONFIG_YAML.stem)
 def main(cfg: DictConfig):
-    """Converts the event-sharded raw data into MEDS events and storing them in patient subsharded flat files.
-
-    All arguments are specified through the command line into the `cfg` object through Hydra.
-
-    The `cfg.stage_cfg` object is a special key that is imputed by OmegaConf to contain the stage-specific
-    configuration arguments based on the global, pipeline-level configuration file. It cannot be overwritten
-    directly on the command line, but can be overwritten implicitly by overwriting components of the
-    `stage_configs.convert_to_sharded_events` key.
-
-
-    This stage has no stage-specific configuration arguments. It does, naturally, require the global,
-    `event_conversion_config_fp` configuration argument to be set to the path of the event conversion yaml
-    file.
-    """
+    """TODO."""
 
     stage_input_dir, partial_metadata_dir, _, _ = stage_init(cfg)
     raw_input_dir = Path(cfg.input_dir)
@@ -394,20 +218,58 @@ def main(cfg: DictConfig):
     event_metadata_configs = list(events_and_metadata_by_metadata_fp.items())
     random.shuffle(event_metadata_configs)
 
+    # Load all codes
+    all_codes = (
+        pl.scan_parquet(stage_input_dir / "**/*.parquet")
+        .select(pl.col("code").unique())
+        .collect()
+        .get_column("code")
+        .to_list()
+    )
+
+    all_out_fps = []
     for input_prefix, event_metadata_cfgs in event_metadata_configs:
         event_metadata_cfgs = copy.deepcopy(event_metadata_cfgs)
 
-        metadata_fp, read_fn = find_metadata_fp(raw_input_dir, input_prefix)
+        metadata_fp, read_fn = get_supported_fp(raw_input_dir, input_prefix)
         out_fp = partial_metadata_dir / f"{input_prefix}.parquet"
         logger.info(f"Extracting metadata from {metadata_fp} and saving to {out_fp}")
 
-        compute_fn = partial(extract_metadata, event_cfg=event_metadata_cfgs)
+        compute_fn = partial(extract_metadata, event_cfg=event_metadata_cfgs, allowed_codes=all_codes)
 
         rwlock_wrap(metadata_fp, out_fp, read_fn, write_lazyframe, compute_fn, do_overwrite=cfg.do_overwrite)
+        all_out_fps.append(out_fp)
 
     logger.info("Extracted metadata for all events. Merging.")
 
-    raise NotImplementedError("This function is not yet implemented.")
+    if cfg.worker != 0:
+        logger.info("Code metadata extraction completed. Exiting")
+        return
+
+    logger.info("Starting reduction process")
+
+    while not all(fp.is_file() for fp in all_out_fps):
+        logger.info("Waiting to begin reduction for all files to be written...")
+        time.sleep(cfg.polling_time)
+
+    start = datetime.now()
+    logger.info("All map shards complete! Starting code metadata reduction computation.")
+    reducer_fn = partial(pl.concat, how="vertical")
+
+    reduced = reducer_fn(*[pl.scan_parquet(fp, glob=False) for fp in all_out_fps])
+    join_cols = ["code", *code_modifier_cols]
+    metadata_cols = [c for c in reduced.columns if c not in join_cols]
+    reduced = reduced.group_by(join_cols).agg(*(pl.col(c) for c in metadata_cols)).collect()
+
+    reducer_fp = Path(cfg.cohort_dir) / "code_metadata.parquet"
+
+    if reducer_fp.exists():
+        logger.info(f"Joining to existing code metadata at {str(reducer_fp.resolve())}")
+        existing = pl.read_parquet(reducer_fp, use_pyarrow=True)
+        reduced = existing.join(reduced, on=join_cols, how="outer")
+
+    pl.write_parquet(reduced, reducer_fp, use_pyarrow=True)
+    logger.info(f"Finished reduction in {datetime.now() - start}")
 
 
 if __name__ == "__main__":
---
+++ src/MEDS_polars_functions/extract/utils.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+import gzip
+import warnings
+from enum import StrEnum
+from pathlib import Path
+
+import polars as pl
+from loguru import logger
+
+
+
+class SupportedFileFormats(StrEnum):
+    """The supported file formats for dataframes we can read in, in priority order.
+
+    The values of the enum are the allowed file suffix for the format.
+    """
+
+    PARQUET = ".parquet"
+    CSV_GZ = ".csv.gz"
+    CSV = ".csv"
+
+
+def scan_csv_gz(fp: Path, **kwargs) -> pl.LazyFrame:
+    with gzip.open(fp, mode="rb") as f:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=UserWarning)
+            return pl.read_csv(f, **kwargs).lazy()
+
+
+READERS = {
+    SupportedFileFormats.PARQUET: pl.scan_parquet,
+    SupportedFileFormats.CSV_GZ: scan_csv_gz,
+    SupportedFileFormats.CSV: pl.scan_csv,
+}
+
+
+DF_T = TypeVar("DF_T")
+
+
+def get_supported_fp(root_dir: Path, file_prefix: str | Path) -> tuple[Path, Callable[[Path], DF_T]]:
+    """This function finds the best file path to read for a given root_dir and prefix.
+
+    Args:
+        root_dir: The root directory to search for files.
+        file_prefix: The file prefix to search for.
+
+    Raises:
+        FileNotFoundError: If no files are found with the given prefix and an allowed suffix.
+
+    Returns:
+        The filepath with the matching prefix and the most preferred allowed suffix and an appropriate reader
+        function for that file type.
+
+    Examples:
+        >>> from tempfile import TemporaryDirectory
+        >>> df = pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, schema={"a": pl.UInt8, "b": pl.Int64})
+        >>> with TemporaryDirectory() as tmpdir:
+        ...     fp = Path(tmpdir) / "test.csv"
+        ...     df.write_csv(fp)
+        ...     scan_with_row_idx(fp, columns=["a"], infer_schema_length=40).collect()
+        shape: (3, 2)
+        ┌─────────────┬─────┐
+        │ __row_idx__ ┆ a   │
+        │ ---         ┆ --- │
+        │ u32         ┆ i64 │
+        ╞═════════════╪═════╡
+        │ 0           ┆ 1   │
+        │ 1           ┆ 2   │
+        │ 2           ┆ 3   │
+        └─────────────┴─────┘
+        >>> with TemporaryDirectory() as tmpdir:
+        ...     fp = Path(tmpdir) / "test.parquet"
+        ...     df.write_parquet(fp)
+        ...     scan_with_row_idx(fp, columns=["a", "b"], infer_schema_length=40).collect()
+        shape: (3, 3)
+        ┌─────────────┬─────┬─────┐
+        │ __row_idx__ ┆ a   ┆ b   │
+        │ ---         ┆ --- ┆ --- │
+        │ u32         ┆ u8  ┆ i64 │
+        ╞═════════════╪═════╪═════╡
+        │ 0           ┆ 1   ┆ 4   │
+        │ 1           ┆ 2   ┆ 5   │
+        │ 2           ┆ 3   ┆ 6   │
+        └─────────────┴─────┴─────┘
+        >>> import gzip
+        >>> with TemporaryDirectory() as tmpdir:
+        ...     fp = Path(tmpdir) / "test.csv.gz"
+        ...     with gzip.open(fp, mode="wb") as f:
+        ...         with warnings.catch_warnings():
+        ...             warnings.simplefilter("ignore", category=UserWarning)
+        ...             df.write_csv(f)
+        ...     scan_with_row_idx(fp, columns=["b"]).collect()
+        shape: (3, 2)
+        ┌─────────────┬─────┐
+        │ __row_idx__ ┆ b   │
+        │ ---         ┆ --- │
+        │ u32         ┆ i64 │
+        ╞═════════════╪═════╡
+        │ 0           ┆ 4   │
+        │ 1           ┆ 5   │
+        │ 2           ┆ 6   │
+        └─────────────┴─────┘
+        >>> with TemporaryDirectory() as tmpdir:
+        ...     fp = Path(tmpdir) / "test.json"
+        ...     df.write_json(fp)
+        ...     scan_with_row_idx(fp, columns=["a", "b"])
+        Traceback (most recent call last):
+            ...
+        ValueError: Unsupported file type: .json
+    """
+
+    for suffix in list(SupportedFileFormats):
+        fp = root_dir / f"{file_prefix}{suffix.value}"
+        if fp.exists():
+            logger.debug(f"Found file: {str(fp.resolve())}")
+            return fp, READERS[suffix]
