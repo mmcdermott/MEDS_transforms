e3309ee9c6c4671a2db7a9a7f04672aa4405603a
Matthew McDermott <mattmcdermott8@gmail.com>
2024-07-19 13:02:13 -0400

    Added starter code for reordering by code patterns

--- src/MEDS_polars_functions/utils.py
+++ src/MEDS_polars_functions/utils.py
@@ -16,6 +16,42 @@ from MEDS_polars_functions import __version__ as package_version
 pl.enable_string_cache()
 
 
+def get_smallest_valid_uint_type(num: int | float | pl.Expr) -> pl.DataType:
+    """Returns the smallest valid unsigned integral type for an ID variable with `num` unique options.
+
+    Args:
+        num: The number of IDs that must be uniquely expressed.
+
+    Raises:
+        ValueError: If there is no unsigned int type big enough to express the passed number of ID
+            variables.
+
+    Examples:
+        >>> get_smallest_valid_uint_type(num=1)
+        UInt8
+        >>> get_smallest_valid_uint_type(num=2**8-1)
+        UInt16
+        >>> get_smallest_valid_uint_type(num=2**16-1)
+        UInt32
+        >>> get_smallest_valid_uint_type(num=2**32-1)
+        UInt64
+        >>> get_smallest_valid_uint_type(num=2**64-1)
+        Traceback (most recent call last):
+            ...
+        ValueError: Value is too large to be expressed as an int!
+    """
+    if num >= (2**64) - 1:
+        raise ValueError("Value is too large to be expressed as an int!")
+    if num >= (2**32) - 1:
+        return pl.UInt64
+    elif num >= (2**16) - 1:
+        return pl.UInt32
+    elif num >= (2**8) - 1:
+        return pl.UInt16
+    else:
+        return pl.UInt8
+
+
 def write_lazyframe(df: pl.LazyFrame, out_fp: Path) -> None:
     if isinstance(df, pl.LazyFrame):
         df = df.collect()
