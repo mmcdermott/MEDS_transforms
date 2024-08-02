b308050a7a716b5a45a885f01ba3ad6969657e36
Matthew McDermott <mattmcdermott8@gmail.com>
2024-08-01 22:53:59 -0400

    Merged new changes and got doctest working for reordering. Still need docstrings and integration tests.

--- src/MEDS_transforms/transforms/reorder_measurements.py
+++ src/MEDS_transforms/transforms/reorder_measurements.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """A polars-to-polars transformation function for filtering patients by sequence length."""
 from collections.abc import Callable
-from importlib.resources import files
 from pathlib import Path
 
 import hydra
 import polars as pl
-from MEDS_polars_functions.mapreduce.mapper import map_over
-from MEDS_polars_functions.utils import get_smallest_valid_uint
+from loguru import logger
 from omegaconf import DictConfig, ListConfig
 
-pl.enable_string_cache()
+from MEDS_transforms import PREPROCESS_CONFIG_YAML
+from MEDS_transforms.mapreduce.mapper import map_over
+from MEDS_transforms.utils import get_smallest_valid_uint_type
 
 
 def reorder_by_code_fntr(
@@ -27,26 +27,27 @@ def reorder_by_code_fntr(
         A function with signature `Callable[[pl.LazyFrame], pl.LazyFrame]` that re-orders the input DataFrame.
 
     Examples:
-        >>> code_metadata_df = pl.DataFrame({
-        ...     "code":       pl.Series(["A",  "A",  "B",  "C"], dtype=pl.Categorical),
-        ...     "modifier1":            [1,    2,    1,    2],
-        ...     "values/n_occurrences": [3,    1,    3,    2],
-        ...     "values/sum":           [0.0,  4.0,  12.0, 2.0],
-        ...     "values/sum_sqd":       [27.0, 16.0, 75.0, 4.0],
-        ... # for clarity: ----- mean = [0.0,  4.0,  4.0,  1.0]
-        ... # for clarity: --- stddev = [3.0,  0.0,  3.0,  1.0]
-        ... })
+        >>> code_metadata_df = pl.DataFrame({"code": ["A", "A", "B", "C"], "modifier1": [1, 2, 1, 2]})
         >>> data = pl.DataFrame({
-        ...     "patient_id":      [1,   1,   2,   2],
-        ...     "code":  pl.Series(["A", "B", "A", "C"], dtype=pl.Categorical),
-        ...     "modifier1":       [1,   1,   2,   2],
-        ... # for clarity: mean    [0.0, 4.0, 4.0, 1.0]
-        ... # for clarity: stddev  [3.0, 3.0, 0.0, 1.0]
-        ...     "numerical_value": [15., 16., 3.9, 1.0],
-        ... }).lazy()
-        >>> stage_cfg = DictConfig({})
+        ...     "patient_id":[1, 1, 2, 2], "time": [1, 1, 1, 1],
+        ...     "code": ["A", "B", "A", "C"], "modifier1": [1, 2, 1, 2]
+        ... })
+        >>> stage_cfg = DictConfig({"ordered_code_patterns": ["B", "A"]})
         >>> fn = reorder_by_code_fntr(stage_cfg, code_metadata_df, ["modifier1"])
-        >>> fn(data).collect()
+        >>> fn(data.lazy()).collect()
+        shape: (4, 4)
+        ┌────────────┬──────┬──────┬───────────┐
+        │ patient_id ┆ time ┆ code ┆ modifier1 │
+        │ ---        ┆ ---  ┆ ---  ┆ ---       │
+        │ i64        ┆ i64  ┆ str  ┆ i64       │
+        ╞════════════╪══════╪══════╪═══════════╡
+        │ 1          ┆ 1    ┆ B    ┆ 2         │
+        │ 1          ┆ 1    ┆ A    ┆ 1         │
+        │ 2          ┆ 1    ┆ A    ┆ 1         │
+        │ 2          ┆ 1    ┆ C    ┆ 2         │
+        └────────────┴──────┴──────┴───────────┘
+
+    TODO: A more complex test with true regexes.
     """
 
     ordered_code_patterns = stage_cfg.get("ordered_code_patterns", None)
@@ -54,25 +55,28 @@ def reorder_by_code_fntr(
         return lambda df: df
 
     if not isinstance(ordered_code_patterns, (list, ListConfig)):
-        raise ValueError("TODO")
+        raise ValueError(
+            f"The 'ordered_code_patterns' field must be a list of strings. Got {ordered_code_patterns}."
+        )
+    for pattern in ordered_code_patterns:
+        if not isinstance(pattern, str):
+            raise ValueError(f"Each element of 'ordered_code_patterns' must be a string. Got {pattern}.")
 
     num_code_patterns = len(ordered_code_patterns)
-    code_pattern_idx_dtype = get_smallest_valid_uint(num_code_patterns + 1)  # TODO: make function
+    code_pattern_idx_dtype = get_smallest_valid_uint_type(num_code_patterns + 1)  # TODO: make function
 
     join_cols = ["code"]
     if code_modifier_columns:
-        raise NotImplementedError("This transformation doesn't currently support code modifier columns.")
+        logger.warning("Code reordering currently only matches against the 'code' column, not code modifiers")
         join_cols.extend(code_modifier_columns)
 
     cols_to_select = ["code"]
     if code_modifier_columns:
         cols_to_select.extend(code_modifier_columns)
 
-    code = pl.col("code").cast(pl.Utf8)
-
     code_order_idx_exprs = pl  # this let's us chain the when/then/otherwise calls equivalently over the loop.
     for i, code_matcher in enumerate(ordered_code_patterns):
-        matcher_expr = code.str.contains(code_matcher)
+        matcher_expr = pl.col("code").str.contains(code_matcher)
         idx_expr = pl.lit(i, dtype=code_pattern_idx_dtype)
         code_order_idx_exprs = code_order_idx_exprs.when(matcher_expr).then(idx_expr)
 
@@ -88,22 +92,21 @@ def reorder_by_code_fntr(
 
         return (
             df.join(code_indices, on=join_cols, how="left", coalesce=True)
-            .sort("patient_id", "timestamp", "code_order_idx", maintain_order=True)
+            .sort("patient_id", "time", "code_order_idx", maintain_order=True)
             .drop("code_order_idx")
         )
 
     return reorder_fn
 
 
-config_yaml = files("MEDS_polars_functions").joinpath("configs/preprocess.yaml")
-
-
-@hydra.main(version_base=None, config_path=str(config_yaml.parent), config_name=config_yaml.stem)
+@hydra.main(
+    version_base=None, config_path=str(PREPROCESS_CONFIG_YAML.parent), config_name=PREPROCESS_CONFIG_YAML.stem
+)
 def main(cfg: DictConfig):
     """TODO: Put script help string here"""
 
     code_metadata = pl.read_parquet(
-        Path(cfg.stage_cfg.metadata_input_dir) / "code_metadata.parquet", use_pyarrow=True
+        Path(cfg.stage_cfg.metadata_input_dir) / "codes.parquet", use_pyarrow=True
     )
 
     map_over(cfg, compute_fn=reorder_by_code_fntr(cfg.stage_cfg, code_metadata))
