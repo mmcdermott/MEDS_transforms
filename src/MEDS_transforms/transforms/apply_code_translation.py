6ecea5a767971dcf93573e3f5040a792a69495e3
Paweł Renc <prenc@mgh.harvard.edu>
2024-10-17 15:07:31 -0400

    Change apply_code_translation to work like a functor

--- src/MEDS_transforms/transforms/apply_code_translation.py
+++ src/MEDS_transforms/transforms/apply_code_translation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """TODO."""
-
+from collections.abc import Callable
 
 import hydra
 import polars as pl
@@ -10,9 +10,9 @@ from MEDS_transforms import PREPROCESS_CONFIG_YAML
 from MEDS_transforms.mapreduce.mapper import map_over
 
 
-def apply_code_translation(
-    df: pl.LazyFrame, code_metadata: pl.DataFrame, code_modifiers: list[str] | None = None
-) -> pl.LazyFrame:
+def apply_code_translation_fntr(
+    stage_cfg: DictConfig, code_metadata: pl.LazyFrame, code_modifiers: list[str] | None = None
+) -> Callable[[pl.LazyFrame], pl.LazyFrame]:
     """In addition, the `code_metadata` dataset should contain information about the codes in the MEDS
     dataset,
 
@@ -37,39 +37,16 @@ def apply_code_translation(
         ...                  "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//C"],
         ...     },
         ...     schema={"subject_id": pl.UInt32, "code": pl.Utf8},
-        ... )
-        >>> empty_metadata = pl.DataFrame(
-        ...     {
-        ...         "code": [],
-        ...         "code/vocab_index": [],
-        ...     },
-        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32},
-        ... )
-        >>> apply_code_translation(df, empty_metadata)
-        shape: (8, 2)
-        ┌────────────┬────────────┐
-        │ subject_id ┆ code       │
-        │ ---        ┆ ---        │
-        │ u32        ┆ str        │
-        ╞════════════╪════════════╡
-        │ 1          ┆ static     │
-        │ 1          ┆ DOB        │
-        │ 1          ┆ ICD//9//A  │
-        │ 1          ┆ ICD//10//A │
-        │ 2          ┆ DOB        │
-        │ 2          ┆ ICD//9//A  │
-        │ 3          ┆ ICD//9//B  │
-        │ 3          ┆ ICD//9//C  │
-        └────────────┴────────────┘
-        >>> metadata = pl.DataFrame(
+        ... ).lazy()
+        >>> stage_cfg = DictConfig({"translation_col": "translated"})
+        >>> metadata_df = pl.DataFrame(
         ...     {
         ...         "code": ["static", "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//C", "ICD//10//A"],
-        ...         "code/vocab_index": [1, 2, 3, 4, 5, 6],
         ...         "translated": [None, None, "ICD//10//A", "ICD//10//B", "ICD//10//C", None],
         ...     },
-        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32, "translated": pl.Utf8},
+        ...     schema={"code": pl.Utf8, "translated": pl.Utf8},
         ... )
-        >>> apply_code_translation(df, metadata)
+        >>> apply_code_translation_fntr(stage_cfg, metadata_df)(df).collect()
         shape: (8, 2)
         ┌────────────┬────────────┐
         │ subject_id ┆ code       │
@@ -85,15 +62,14 @@ def apply_code_translation(
         │ 3          ┆ ICD//10//B │
         │ 3          ┆ ICD//10//C │
         └────────────┴────────────┘
-        >>> metadata = pl.DataFrame(
+        >>> metadata_df = pl.DataFrame(
         ...     {
         ...         "code": ["static", "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//B", "ICD//10//A"],
-        ...         "code/vocab_index": [1, 2, 3, 4, 5, 6],
         ...         "translated": [None, None, "ICD//10//A", "ICD//10//B", "ICD//10//C", None],
         ...     },
-        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32, "translated": pl.Utf8},
+        ...     schema={"code": pl.Utf8, "translated": pl.Utf8},
         ... )
-        >>> apply_code_translation(df, metadata)
+        >>> apply_code_translation_fntr(stage_cfg, metadata_df)(df).collect()
         shape: (9, 2)
         ┌────────────┬────────────┐
         │ subject_id ┆ code       │
@@ -111,8 +87,21 @@ def apply_code_translation(
         │ 3          ┆ ICD//9//C  │
         └────────────┴────────────┘
     """
+    translation_col = stage_cfg.get("translation_col", None)
+    if translation_col is None:
+        return lambda df: df
 
-    return df
+    if translation_col not in code_metadata.columns:
+        raise ValueError(f"Column '{translation_col}' not found in code metadata.")
+
+    def apply_code_translation_fn(df: pl.LazyFrame) -> pl.LazyFrame:
+        return (
+            df.join(code_metadata.select(translation_col, "code").lazy(), on="code", how="left")
+            .with_columns(code=pl.coalesce(translation_col, "code"))
+            .drop(translation_col)
+        )
+
+    return apply_code_translation_fn
 
 
 @hydra.main(
@@ -121,7 +110,7 @@ def apply_code_translation(
 def main(cfg: DictConfig):
     """TODO."""
 
-    map_over(cfg, compute_fn=apply_code_translation)
+    map_over(cfg, compute_fn=apply_code_translation_fntr)
 
 
 if __name__ == "__main__":  # pragma: no cover
