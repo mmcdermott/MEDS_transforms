27cdca81cae01cc3d2fbada68adf88117b43bbd8
Paweł Renc <prenc@mgh.harvard.edu>
2024-10-16 21:23:39 -0400

    Add files and doc-tests of the second step of the code translation

---
+++ src/MEDS_transforms/transforms/apply_code_translation.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+"""TODO."""
+
+
+import hydra
+import polars as pl
+from omegaconf import DictConfig
+
+from MEDS_transforms import PREPROCESS_CONFIG_YAML
+from MEDS_transforms.mapreduce.mapper import map_over
+
+
+def apply_code_translation(
+    df: pl.LazyFrame, code_metadata: pl.DataFrame, code_modifiers: list[str] | None = None
+) -> pl.LazyFrame:
+    """In addition, the `code_metadata` dataset should contain information about the codes in the MEDS
+    dataset,
+
+    including the mandatory columns:
+      - `code` (`str`)
+      - `code/vocab_index` (`int`)
+      - Any `code_modifiers` columns, if specified
+
+    Args:
+        df: The MEDS dataset to normalize. See above for the expected schema.
+        code_metadata: Metadata about the codes in the MEDS dataset. See above for the expected schema.
+        code_modifiers: Additional columns to join on, which will be discarded from the output dataframe.
+
+    Returns:
+        The translated MEDS dataset, with the schema described above.
+
+    Examples:
+        >>> df = pl.DataFrame(
+        ...     {
+        ...         "subject_id": [1, 1, 1, 1, 2, 2, 3, 3],
+        ...         "code": ["static", "DOB", "ICD//9//A", "ICD//10//A",
+        ...                  "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//C"],
+        ...     },
+        ...     schema={"subject_id": pl.UInt32, "code": pl.Utf8},
+        ... )
+        >>> empty_metadata = pl.DataFrame(
+        ...     {
+        ...         "code": [],
+        ...         "code/vocab_index": [],
+        ...     },
+        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32},
+        ... )
+        >>> apply_code_translation(df, empty_metadata)
+        shape: (8, 2)
+        ┌────────────┬────────────┐
+        │ subject_id ┆ code       │
+        │ ---        ┆ ---        │
+        │ u32        ┆ str        │
+        ╞════════════╪════════════╡
+        │ 1          ┆ static     │
+        │ 1          ┆ DOB        │
+        │ 1          ┆ ICD//9//A  │
+        │ 1          ┆ ICD//10//A │
+        │ 2          ┆ DOB        │
+        │ 2          ┆ ICD//9//A  │
+        │ 3          ┆ ICD//9//B  │
+        │ 3          ┆ ICD//9//C  │
+        └────────────┴────────────┘
+        >>> metadata = pl.DataFrame(
+        ...     {
+        ...         "code": ["static", "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//C", "ICD//10//A"],
+        ...         "code/vocab_index": [1, 2, 3, 4, 5, 6],
+        ...         "translated": [None, None, "ICD//10//A", "ICD//10//B", "ICD//10//C", None],
+        ...     },
+        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32, "translated": pl.Utf8},
+        ... )
+        >>> apply_code_translation(df, metadata)
+        shape: (8, 2)
+        ┌────────────┬────────────┐
+        │ subject_id ┆ code       │
+        │ ---        ┆ ---        │
+        │ u32        ┆ str        │
+        ╞════════════╪════════════╡
+        │ 1          ┆ static     │
+        │ 1          ┆ DOB        │
+        │ 1          ┆ ICD//10//A │
+        │ 1          ┆ ICD//10//A │
+        │ 2          ┆ DOB        │
+        │ 2          ┆ ICD//10//A │
+        │ 3          ┆ ICD//10//B │
+        │ 3          ┆ ICD//10//C │
+        └────────────┴────────────┘
+        >>> metadata = pl.DataFrame(
+        ...     {
+        ...         "code": ["static", "DOB", "ICD//9//A", "ICD//9//B", "ICD//9//B", "ICD//10//A"],
+        ...         "code/vocab_index": [1, 2, 3, 4, 5, 6],
+        ...         "translated": [None, None, "ICD//10//A", "ICD//10//B", "ICD//10//C", None],
+        ...     },
+        ...     schema={"code": pl.Utf8, "code/vocab_index": pl.UInt32, "translated": pl.Utf8},
+        ... )
+        >>> apply_code_translation(df, metadata)
+        shape: (9, 2)
+        ┌────────────┬────────────┐
+        │ subject_id ┆ code       │
+        │ ---        ┆ ---        │
+        │ u32        ┆ str        │
+        ╞════════════╪════════════╡
+        │ 1          ┆ static     │
+        │ 1          ┆ DOB        │
+        │ 1          ┆ ICD//10//A │
+        │ 1          ┆ ICD//10//A │
+        │ 2          ┆ DOB        │
+        │ 2          ┆ ICD//10//A │
+        │ 3          ┆ ICD//10//B │
+        │ 3          ┆ ICD//10//C │
+        │ 3          ┆ ICD//9//C  │
+        └────────────┴────────────┘
+    """
+
+    return df
+
+
+@hydra.main(
+    version_base=None, config_path=str(PREPROCESS_CONFIG_YAML.parent), config_name=PREPROCESS_CONFIG_YAML.stem
+)
+def main(cfg: DictConfig):
+    """TODO."""
+
+    map_over(cfg, compute_fn=apply_code_translation)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
