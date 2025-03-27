ae7b8169662775df2e3f7b8b3f6f858b0514dd64
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-27 10:55:03 -0400

    Reworked stage decorator to be more flexible in usage and with clear guidance on how to use it. Should permit decorator usage throughout.

--- src/MEDS_transforms/stage.py
+++ src/MEDS_transforms/stage.py
@@ -15,6 +15,8 @@ from .mapreduce import ANY_COMPUTE_FN_T, map_stage, mapreduce_stage
 
 logger = logging.getLogger(__name__)
 
+MAIN_FN_T = Callable[[DictConfig], None]
+
 
 class StageType(StrEnum):
     """The types of stages MEDS-Transforms supports.
@@ -33,41 +35,15 @@ class StageType(StrEnum):
     MAIN = "main"
 
 
-def make_main_fn(
-    map_fn: ANY_COMPUTE_FN_T | None = None,
-    reduce_fn: ANY_COMPUTE_FN_T | None = None,
-) -> Callable[[DictConfig], None]:
-    if map_fn is None:
-        raise ValueError("map_fn must be provided")
-
-    if reduce_fn is None:
-        docstring = inspect.getdoc(map_fn) or ""
-
-        @functools.wraps(map_fn)
-        def main_fn(cfg: DictConfig):
-            return map_stage(cfg, map_fn)
-
-    else:
-        docstring = (
-            f"Map Stage:\n{inspect.getdoc(map_fn) or ''}\n\n"
-            f"Reduce stage:\n{inspect.getdoc(reduce_fn) or ''}"
-        )
-
-        def main_fn(cfg: DictConfig):
-            return mapreduce_stage(cfg, map_fn, reduce_fn)
-
-    main_fn.__doc__ = docstring
-    return main_fn
-
-
-def MEDS_transforms_stage(
-    main_fn: Callable[[DictConfig], None] | None = None,
+def get_stage_main(
+    *,
+    main_fn: MAIN_FN_T | None = None,
     map_fn: ANY_COMPUTE_FN_T | None = None,
     reduce_fn: ANY_COMPUTE_FN_T | None = None,
     config_path: Path | None = None,
     stage_name: str | None = None,
     stage_docstring: str | None = None,
-):
+) -> MAIN_FN_T:
     """Wraps or returns a function that can serve as the main function for a stage."""
 
     if main_fn is not None:
@@ -93,12 +69,27 @@ def MEDS_transforms_stage(
     if stage_name is None:
         stage_name = (main_fn or map_fn).__module__.split(".")[-1]
 
-    if mode != StageType.MAIN:
-        main_fn = make_main_fn(map_fn, reduce_fn)
-        main_fn.__name__ = stage_name
+    match mode:
+        case StageType.MAIN:
+            stage_docstring = stage_docstring or inspect.getdoc(main_fn) or ""
+        case StageType.MAP:
+            stage_docstring = stage_docstring or inspect.getdoc(map_fn) or ""
 
-    if stage_docstring is None:
-        stage_docstring = inspect.getdoc(main_fn) or ""
+            @functools.wraps(map_fn)
+            def main_fn(cfg: DictConfig):
+                return map_stage(cfg, map_fn)
+
+        case StageType.MAPREDUCE:
+            stage_docstring = stage_docstring or (
+                f"Map Stage:\n{inspect.getdoc(map_fn) or ''}\n\n"
+                f"Reduce stage:\n{inspect.getdoc(reduce_fn) or ''}"
+            )
+
+            def main_fn(cfg: DictConfig):
+                return mapreduce_stage(cfg, map_fn, reduce_fn)
+
+    main_fn.__name__ = stage_name
+    main_fn.__doc__ = stage_docstring
 
     hydra_wraped_main = hydra_wrapper(main_fn)
 
@@ -111,3 +102,58 @@ def MEDS_transforms_stage(
         return hydra_wraped_main(*args, **kwargs)
 
     return wrapped_main
+
+
+def MEDS_transforms_stage(*args, **kwargs):
+    """This is a decorator used to define and register a MEDS-Transforms stage of any variety.
+
+    It can be used in a variety of ways, depending on the arguments provided and the manner of invocation:
+
+      1. If used as a decorator without arguments to a single function named `main`, it will define a
+         `StageType.MAIN` stage using that function, and the return value will be the target executable main
+         function itself (which will have an identical signature to the passed function). The function will
+         not have other methods registered to it.
+      2. If used as a decorator without arguments to a single function _not_ named `main`, it will define a
+         `StageType.MAP` stage using that function or functor as the mapper. The return value will be a new
+         function object that has the same signature as the passed function, but with a new method `main`
+         attached to it that contains the main CLI entry point for the stage and can be invoked with hydra.
+      3. If used as a decorator with keyword arguments to a single function named `main`, and the arguments do
+         not include `map_fn` or `reduce_fn`, it will define a `StageType.MAIN` stage using that function, and
+         the other keyword arguments will overwrite the defaults used in the definition of the stage.
+      4. If used as a decorator with keyword arguments to a single function _not_ named `main`, and the
+         arguments do not include `map_fn` or `reduce_fn`, it will define a `StageType.MAP` stage using that
+         function or functor as the mapper, and the other keyword arguments will overwrite the defaults used
+         in the definition of that stage.
+      5. If used as a decorator with keyword arguments to a single function regardless of the name, and the
+         keyword arguments define a `reduce_fn` but do not define a `map_fn`, then a `StageType.MAPREDUCE`
+         stage will be created and attached to the function via the `main` entry point, with the passed
+         function treated as the mapper and the `reduce_fn` as the reducer. The other keyword arguments will
+         be used as normal. The function decorated will be returned without alteration to its signature.
+      6. If used as a decorator to a single function with the `map_fn` keyword argument provided, it will
+         throw an error.
+      7. If called directly with only keyword arguments, not as a decorator to a function, with either
+         `main_fn`, `map_fn`, or both `map_fn` and `reduce_fn` defined, it will produce the associated stage
+         for those settings and return the main function directly, without modifying any passed function.
+      8. If called with multiple position arguments, it will throw an error.
+    """
+
+    if len(args) > 1:
+        raise ValueError(
+            f"MEDS_transforms_stage can only be used with at most a single positional arg. Got {len(args)}"
+        )
+    if len(args) == 0:
+        return get_stage_main(**kwargs)
+
+    fn = args[0]
+    if not inspect.isfunction(fn):
+        raise TypeError(f"First argument must be a function. Got {type(fn)}")
+
+    if "main_fn" in kwargs or "map_fn" in kwargs:
+        raise ValueError("Cannot provide main_fn or map_fn when using as a decorator.")
+
+    if fn.__name__ == "main" and "reduce_fn" not in kwargs:
+        return get_stage_main(main_fn=fn, **kwargs)
+
+    main_fn = get_stage_main(main_fn=None, map_fn=fn, **kwargs)
+    fn.main = main_fn
+    return fn
