b85e3f166a7b329ad78455a08be38b7bad877e16
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-27 14:50:31 -0400

    Added doctests for decorator

--- src/MEDS_transforms/stage.py
+++ src/MEDS_transforms/stage.py
@@ -1,7 +1,5 @@
 """Functions for registering and defining MEDS-transforms stages."""
 
-from __future__ import annotations
-
 import functools
 import inspect
 import logging
@@ -39,7 +37,7 @@ class StageType(StrEnum):
     @classmethod
     def from_fns(
         cls, main_fn: MAIN_FN_T | None, map_fn: ANY_COMPUTE_FN_T | None, reduce_fn: ANY_COMPUTE_FN_T | None
-    ) -> StageType:
+    ) -> "StageType":
         """Determines the stage type based on the provided functions.
 
         Args:
@@ -55,13 +53,13 @@ class StageType(StrEnum):
             ValueError: If the provided functions do not correspond to a valid stage type.
 
         Examples:
-            >>> def main_fn(cfg: DictConfig):
+            >>> def main(cfg: DictConfig):
             ...     pass
             >>> def map_fn(cfg: DictConfig, stage_cfg: DictConfig):
             ...     pass
             >>> def reduce_fn(cfg: DictConfig, stage_cfg: DictConfig):
             ...     pass
-            >>> StageType.from_fns(main_fn=main_fn, map_fn=None, reduce_fn=None)
+            >>> StageType.from_fns(main_fn=main, map_fn=None, reduce_fn=None)
             <StageType.MAIN: 'main'>
             >>> StageType.from_fns(main_fn=None, map_fn=map_fn, reduce_fn=None)
             <StageType.MAP: 'map'>
@@ -71,7 +69,7 @@ class StageType(StrEnum):
             Traceback (most recent call last):
                 ...
             ValueError: Either main_fn or map_fn/reduce_fn must be provided.
-            >>> StageType.from_fns(main_fn=main_fn, map_fn=map_fn, reduce_fn=reduce_fn)
+            >>> StageType.from_fns(main_fn=main, map_fn=map_fn, reduce_fn=reduce_fn)
             Traceback (most recent call last):
                 ...
             ValueError: Only one of main_fn or map_fn/reduce_fn should be provided.
@@ -124,6 +122,9 @@ def get_stage_main(
             def main_fn(cfg: DictConfig):
                 return map_stage(cfg, map_fn)
 
+            main_fn.__name__ = stage_name
+            main_fn.__doc__ = stage_docstring
+
         case StageType.MAPREDUCE:
             stage_docstring = stage_docstring or (
                 f"Map Stage:\n{inspect.getdoc(map_fn) or ''}\n\n"
@@ -133,8 +134,8 @@ def get_stage_main(
             def main_fn(cfg: DictConfig):
                 return mapreduce_stage(cfg, map_fn, reduce_fn)
 
-    main_fn.__name__ = stage_name
-    main_fn.__doc__ = stage_docstring
+            main_fn.__name__ = stage_name
+            main_fn.__doc__ = stage_docstring
 
     hydra_wraped_main = hydra_wrapper(main_fn)
 
@@ -180,25 +181,196 @@ def MEDS_transforms_stage(*args, **kwargs):
          `main_fn`, `map_fn`, or both `map_fn` and `reduce_fn` defined, it will produce the associated stage
          for those settings and return the main function directly, without modifying any passed function.
       8. If called with multiple position arguments, it will throw an error.
+
+    In some cases, the attached main function will have a docstring and name set based on the passed or
+    inferred stage name and stage docstring. Any non-main function returned will be unmodified in these
+    aspects.
+
+    Args:
+        *args: Positional arguments. If used as a decorator, this should be a single function.
+        **kwargs: Keyword arguments. These can include `main_fn`, `map_fn`, `reduce_fn`, `config_path`,
+            `stage_name`, and `stage_docstring`. Other keyword arguments will cause an error. Not all keyword
+            arguments are required for all usages of the decorator.
+
+    Returns:
+        Callable: May be the original function passed in with a `main` method attached or a new function. See
+            the description above for details.
+
+    Raises:
+        ValueError: If multiple positional arguments are passed or if the function is not callable.
+        TypeError: If the first positional argument is not a function.
+        ValueError: If both `main_fn` and `map_fn` or `reduce_fn` are provided.
+
+    Examples:
+        >>> def main(cfg: DictConfig):
+        ...     '''base main docstring'''
+        ...     return "main"
+        >>> def map_fn(cfg: DictConfig, stage_cfg: DictConfig):
+        ...     '''base map docstring'''
+        ...     return "map"
+        >>> def reduce_fn(cfg: DictConfig, stage_cfg: DictConfig):
+        ...     '''base reduce docstring'''
+        ...     return "reduce"
+
+        When used with only keyword arguments that fully define a function, the output main function will be
+        returned, and the input functions will be unmodified.
+
+        >>> fn = MEDS_transforms_stage(main_fn=main)
+        >>> fn(DictConfig({}))
+        'main'
+        >>> hasattr(fn, "main")
+        False
+        >>> hasattr(main, "main")
+        False
+        >>> fn.__name__
+        'main'
+        >>> fn.__doc__
+        'base main docstring'
+
+        If we do this with the map or mapreduce stage set-up, the returned function can't be executed as it
+        will try to run our actual mapreduce stages, but we show the surrounding properties:
+
+        >>> fn = MEDS_transforms_stage(map_fn=map_fn)
+        >>> hasattr(fn, "main")
+        False
+        >>> fn.__name__ # Inferred from the source module name
+        'stage'
+        >>> fn.__doc__ # Inferred from the map function docstring
+        'base map docstring'
+        >>> fn = MEDS_transforms_stage(map_fn=map_fn, reduce_fn=reduce_fn)
+        >>> hasattr(fn, "main")
+        False
+        >>> fn.__name__ # Inferred from the source module name
+        'stage'
+        >>> print(fn.__doc__) # Inferred from the map function docstring
+        Map Stage:
+        base map docstring
+        <BLANKLINE>
+        Reduce stage:
+        base reduce docstring
+        >>> map_fn.__name__
+        'map_fn'
+        >>> map_fn.__doc__
+        'base map docstring'
+        >>> reduce_fn.__name__
+        'reduce_fn'
+        >>> reduce_fn.__doc__
+        'base reduce docstring'
+
+        If used as a decorator (equivalently, if passed a single positional argument) to a function named
+        `main`, it will define a `StageType.MAIN` stage using that.
+
+        >>> fn = MEDS_transforms_stage(main)
+        >>> fn(DictConfig({}))
+        'main'
+        >>> hasattr(fn, "main")
+        False
+        >>> fn.__name__
+        'main'
+        >>> fn.__doc__
+        'base main docstring'
+
+        If used as a decorator (equivalently, if passed a single positional argument) to a function not named
+        `main`, it will define a map or mapreduce stage as appropriate, using the decorated function as a
+        mapper, and return the passed function with a new method `main` attached to it that contains the main
+        CLI entry point for the stage. We can't run the 'main' function here as it implements other stage
+        operations, but we can check that it has been added to the function.
+
+        >>> fn = MEDS_transforms_stage(map_fn)
+        >>> fn(cfg={}, stage_cfg={})
+        'map'
+        >>> hasattr(fn, "main")
+        True
+        >>> fn.__name__
+        'map_fn'
+        >>> fn.__doc__
+        'base map docstring'
+        >>> fn.main.__name__ # Inferred from the source module name
+        'stage'
+        >>> fn.main.__doc__ # Inferred from the map function docstring
+        'base map docstring'
+        >>> fn = MEDS_transforms_stage(map_fn, reduce_fn=reduce_fn)
+        >>> fn(cfg={}, stage_cfg={}) # Still the mapper -- that is the "root" function
+        'map'
+        >>> hasattr(fn, "main")
+        True
+        >>> fn.main.__name__ # Inferred from the source module name
+        'stage'
+        >>> print(fn.main.__doc__) # Inferred from the map function docstring
+        Map Stage:
+        base map docstring
+        <BLANKLINE>
+        Reduce stage:
+        base reduce docstring
+
+        Keyword arguments can be used to specify the config path, stage name, and docstring. These are used to
+        modify the returned stage name and docstring, and the config path is used to set up the hydra wrapper:
+
+        >>> @MEDS_transforms_stage(config_path=Path("f.yaml"), stage_name="bar", stage_docstring="baz")
+        ... def special_map(cfg: DictConfig):
+        ...     '''Not baz'''
+        ...     return "map"
+        >>> special_map.main.__name__
+        'bar'
+        >>> special_map.main.__doc__
+        'baz'
+
+        If specified with no positional arguments and only non-determining keyword arguments (meaning no main
+        or map function), then a decorator is returned that itself takes a function:
+        >>> fntr = MEDS_transforms_stage(config_path=Path("f.yaml"), stage_name="bar", stage_docstring="baz")
+        >>> fn = fntr(main)
+        >>> fn(DictConfig({}))
+        'main'
+        >>> hasattr(fn, "main")
+        False
+        >>> fn = fntr(map_fn)
+        >>> fn(cfg={}, stage_cfg={})
+        'map'
+        >>> hasattr(fn, "main")
+        True
+
+        Errors are raised if the function is not callable or if multiple positional arguments are passed or if
+        both `main_fn` and `map_fn` or `reduce_fn` are provided.
+
+        >>> MEDS_transforms_stage("foo", "bar")
+        Traceback (most recent call last):
+            ...
+        ValueError: MEDS_transforms_stage can only be used with at most a single positional arg. Got 2
+        >>> MEDS_transforms_stage("foo")
+        Traceback (most recent call last):
+            ...
+        TypeError: First argument must be a function. Got <class 'str'>
+        >>> MEDS_transforms_stage(main, map_fn=map_fn)
+        Traceback (most recent call last):
+            ...
+        ValueError: Cannot provide main_fn or map_fn kwargs when using as a decorator.
+        >>> MEDS_transforms_stage(map_fn, main_fn=main)
+        Traceback (most recent call last):
+            ...
+        ValueError: Cannot provide main_fn or map_fn kwargs when using as a decorator.
     """
+
+    def decorator(fn: Callable, **kwargs):
+        if not inspect.isfunction(fn):
+            raise TypeError(f"First argument must be a function. Got {type(fn)}")
+
+        if "main_fn" in kwargs or "map_fn" in kwargs:
+            raise ValueError("Cannot provide main_fn or map_fn kwargs when using as a decorator.")
+
+        if (fn.__name__ == "main") and ("reduce_fn" not in kwargs):
+            return get_stage_main(main_fn=fn, **kwargs)
+
+        main_fn = get_stage_main(main_fn=None, map_fn=fn, **kwargs)
+        fn.main = main_fn
+        return fn
 
     if len(args) > 1:
         raise ValueError(
             f"MEDS_transforms_stage can only be used with at most a single positional arg. Got {len(args)}"
         )
     if len(args) == 0:
-        return get_stage_main(**kwargs)
-
-    fn = args[0]
-    if not inspect.isfunction(fn):
-        raise TypeError(f"First argument must be a function. Got {type(fn)}")
-
-    if "main_fn" in kwargs or "map_fn" in kwargs:
-        raise ValueError("Cannot provide main_fn or map_fn when using as a decorator.")
-
-    if fn.__name__ == "main" and "reduce_fn" not in kwargs:
-        return get_stage_main(main_fn=fn, **kwargs)
-
-    main_fn = get_stage_main(main_fn=None, map_fn=fn, **kwargs)
-    fn.main = main_fn
-    return fn
+        if "main_fn" in kwargs or "map_fn" in kwargs:
+            return get_stage_main(**kwargs)
+        return functools.partial(decorator, **kwargs)
+    else:
+        return decorator(args[0], **kwargs)
