25ce93df1b47f19d057239a68ebda53aec8b54be
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-28 16:15:04 -0400

    Added capability to have unique input directories and added a reorder measurements test.

--- tests/test_registered_stages.py
+++ tests/test_registered_stages.py
@@ -1,3 +1,4 @@
+import tempfile
 from importlib.resources import files
 from pathlib import Path
 
@@ -25,19 +26,32 @@ def test_registered_stages(simple_static_MEDS: Path, stage: str):
     examples_dir = files(ep_package).joinpath("static_data_examples") / stage
 
     stage_cfg_fp = examples_dir / "cfg.yaml"
+    in_fp = examples_dir / "in.yaml"
     want_fp = examples_dir / "out.yaml"
 
     if not want_fp.is_file():
         pytest.skip(f"Skipping {stage} test because example output not provided.")
 
+    if in_fp.is_file():
+        in_data = MEDSDataset.from_yaml(in_fp)
+        tempdir = tempfile.TemporaryDirectory()
+        input_dir = Path(tempdir.name)
+        in_data.write(input_dir)
+    else:
+        input_dir = simple_static_MEDS
+
     want_data = MEDSDataset.from_yaml(want_fp)
 
     stage_kwargs = OmegaConf.to_container(OmegaConf.load(stage_cfg_fp))
 
-    MEDS_transforms_pipeline_tester(
-        script=CMD_PATTERN.format(stage_name=stage),
-        stage_name=stage,
-        stage_kwargs=stage_kwargs,
-        want_outputs={f"data/{k}": v for k, v in want_data._pl_shards.items()},
-        input_dir=simple_static_MEDS,
-    )
+    try:
+        MEDS_transforms_pipeline_tester(
+            script=CMD_PATTERN.format(stage_name=stage),
+            stage_name=stage,
+            stage_kwargs=stage_kwargs,
+            want_outputs={f"data/{k}": v for k, v in want_data._pl_shards.items()},
+            input_dir=input_dir,
+        )
+    finally:
+        if input_dir != simple_static_MEDS:
+            tempdir.cleanup()
