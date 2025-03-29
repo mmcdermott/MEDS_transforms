e9a491a9b31285b9ab016ceccc07dfbe373fac8e
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-29 11:04:09 -0400

    Started to add aggregation test, but it doesn't work as the input format contains nested fields.

--- tests/test_registered_stages.py
+++ tests/test_registered_stages.py
@@ -1,7 +1,15 @@
+import copy
 import tempfile
 from dataclasses import dataclass
 from importlib.resources import files
 from pathlib import Path
+
+from yaml import load as load_yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:  # pragma: no cover
+    from yaml import Loader
 
 import polars as pl
 import pytest
@@ -21,13 +29,27 @@ DO_USE_YAML_KEY = "__do_use_yaml"
 
 @dataclass
 class StageExample:
-    out_data: MEDSDataset
     stage_cfg: dict
+    want_data: MEDSDataset | None = None
+    want_metadata: pl.DataFrame | None = None
     in_data: MEDSDataset | None = None
     do_use_yaml: bool = False
 
+    def __post_init__(self):
+        if self.want_data is None and self.want_metadata is None:
+            raise ValueError("Either want_data or want_metadata must be provided.")
+
 
 NAMED_EXAMPLES = dict[str, StageExample]
+
+
+def read_metadata_only(fp: Path, **schema_updates) -> pl.DataFrame:
+    data = load_yaml(fp.read_text(), Loader=Loader)
+    assert len(data) == 1
+    key = list(data.keys())[0]
+    assert key == code_metadata_filepath
+
+    return MEDSDataset.parse_csv(data[key], **copy.deepcopy(schema_updates))
 
 
 def parse_example_dir(example_dir: Path, **schema_updates) -> StageExample:
@@ -40,13 +62,25 @@ def parse_example_dir(example_dir: Path, **schema_updates) -> StageExample:
     if not want_fp.is_file():
         raise FileNotFoundError(f"Output file not found: {want_fp}")
 
-    out_data = MEDSDataset.from_yaml(want_fp, **schema_updates)
+    try:
+        want_data = MEDSDataset.from_yaml(want_fp, **copy.deepcopy(schema_updates))
+        want_metadata = None
+    except ValueError as e:
+        if "No data shards found in YAML" not in str(e):
+            raise e
+        want_data = None
+        try:
+            want_metadata = read_metadata_only(want_fp, **copy.deepcopy(schema_updates))
+        except Exception as e2:
+            raise e2 from e
+
     in_data = MEDSDataset.from_yaml(in_fp) if in_fp.is_file() else None
     stage_cfg = OmegaConf.to_container(OmegaConf.load(stage_cfg_fp)) if stage_cfg_fp.is_file() else {}
     do_use_yaml = stage_cfg.pop(DO_USE_YAML_KEY, False)
 
     return StageExample(
-        out_data=out_data,
+        want_data=want_data,
+        want_metadata=want_metadata,
         stage_cfg=stage_cfg,
         in_data=in_data,
         do_use_yaml=do_use_yaml,
@@ -54,11 +88,8 @@ def parse_example_dir(example_dir: Path, **schema_updates) -> StageExample:
 
 
 def is_example_dir(path: Path) -> bool:
-    try:
-        parse_example_dir(path)
-        return True
-    except FileNotFoundError:
-        return False
+    want_fp = path / "out.yaml"
+    return want_fp.is_file()
 
 
 def get_nested_test_cases(
@@ -77,13 +108,13 @@ def get_nested_test_cases(
         return test_cases
 
     if is_example_dir(example_dir):
-        test_cases[prefix] = parse_example_dir(example_dir, **schema_updates)
+        test_cases[prefix] = parse_example_dir(example_dir, **copy.deepcopy(schema_updates))
         return test_cases
 
     for path in example_dir.iterdir():
         if path.is_dir():
             nested_prefix = f"{prefix}/{path.name}"
-            get_nested_test_cases(path, nested_prefix, test_cases, **schema_updates)
+            get_nested_test_cases(path, nested_prefix, test_cases, **copy.deepcopy(schema_updates))
 
     return test_cases
 
@@ -97,7 +128,21 @@ def test_registered_stages(simple_static_MEDS: Path, stage: str):
 
     examples_dir = files(ep_package).joinpath("static_data_examples") / stage
 
-    schema_updates = {} if stage != "normalization" else {"code": pl.Int64, "numeric_value": pl.Float64}
+    match stage:
+        case "normalization":
+            schema_updates = {"code": pl.Int64, "numeric_value": pl.Float64}
+        case "aggregate_code_metadata":
+            schema_updates = {
+                "values/quantiles": pl.Struct(
+                    {
+                        "values/quantile/0.25": pl.Float32,
+                        "values/quantile/0.5": pl.Float32,
+                        "values/quantile/0.75": pl.Float32,
+                    }
+                )
+            }
+        case _:
+            schema_updates = {}
 
     examples = get_nested_test_cases(examples_dir, stage, **schema_updates)
 
@@ -123,14 +168,17 @@ def test_registered_stages(simple_static_MEDS: Path, stage: str):
         else:
             input_dir = simple_static_MEDS
 
-        want_data = example.out_data
+        if example.want_data is not None:
+            want_outputs = {f"data/{k}": v for k, v in example.want_data._pl_shards.items()}
+        else:
+            want_outputs = {code_metadata_filepath: example.want_metadata}
 
         try:
             MEDS_transforms_pipeline_tester(
                 script=CMD_PATTERN.format(stage_name=stage),
                 stage_name=stage,
                 stage_kwargs=example.stage_cfg,
-                want_outputs={f"data/{k}": v for k, v in want_data._pl_shards.items()},
+                want_outputs=want_outputs,
                 input_dir=input_dir,
                 do_use_config_yaml=example.do_use_yaml,
                 test_name=name,
