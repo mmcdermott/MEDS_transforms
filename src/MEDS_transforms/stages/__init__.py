7e460b0ecf211efa27b6a5e12764474f2ac1d1d2
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-30 10:08:57 -0400

    Removed unnecessary comment.

--- src/MEDS_transforms/stages/__init__.py
+++ src/MEDS_transforms/stages/__init__.py
@@ -1,6 +1,11 @@
+# Import the stage registration helper
+from .base import MEDS_transforms_stage
+
+__all__ = ["MEDS_transforms_stage"]
+
+# Import individual stages. These aren't exposed via __all__, but are importable individually
 from .add_time_derived_measurements import main as add_time_derived_measurements  # noqa: F401
 from .aggregate_code_metadata import main as aggregate_code_metadata  # noqa: F401
-from .base import MEDS_transforms_stage  # noqa: F401
 from .extract_values import main as extract_values  # noqa: F401
 from .filter_measurements import main as filter_measurements  # noqa: F401
 from .filter_subjects import main as filter_subjects  # noqa: F401
@@ -9,5 +14,3 @@ from .normalization import main as normalization  # noqa: F401
 from .occlude_outliers import main as occlude_outliers  # noqa: F401
 from .reorder_measurements import main as reorder_measurements  # noqa: F401
 from .reshard_to_split import main as reshard_to_split  # noqa: F401
-
-__all__ = ["MEDS_transforms_stage"]
