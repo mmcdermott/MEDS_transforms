e9c523b1f29f1df9714babb93848c193cf8c5127
Matthew McDermott <mattmcdermott8@gmail.com>
2025-04-08 13:03:55 -0400

    Added a stage config object.

--- src/MEDS_transforms/configs/pipeline.py
+++ src/MEDS_transforms/configs/pipeline.py
@@ -7,6 +7,7 @@ import logging
 import os
 from importlib.resources import files
 from pathlib import Path
+from typing import ClassVar
 
 from hydra.core.config_store import ConfigStore
 from omegaconf import DictConfig, OmegaConf
@@ -68,6 +69,207 @@ def resolve_pkg_path(pkg_path: str) -> Path:
 
 
 @dataclasses.dataclass
+class StageConfig:
+    """A configuration class for a stage in the MEDS-transforms pipeline.
+
+    This class is used to define the structure of a stage configuration file. It is not intended to be
+    instantiated directly; rather, it is used as a base class for specific stage configurations.
+
+    Attributes:
+        name: The name of the stage.
+        config: The configuration for the stage.
+    """
+
+    name: str
+    base_stage: str | None = None
+    config: dict | DictConfig | None = None
+
+    META_KEYS: ClassVar[set[str]] = {
+        "_base_stage",
+    }
+
+    @classmethod
+    def _is_meta_key(cls, key: str) -> bool:
+        """Checks if a key is a meta-key for the configuration.
+
+        Meta-keys are keys that re in the `cls.META_KEYS` set.
+
+        Args:
+            key: The key to check.
+
+        Returns:
+            True if the key is a meta-key, False otherwise.
+
+        Examples:
+            >>> StageConfig._is_meta_key("_base_stage")
+            True
+            >>> StageConfig._is_meta_key("base_stage")
+            False
+            >>> StageConfig._is_meta_key("_foo")
+            False
+        """
+        return key in cls.META_KEYS
+
+    @classmethod
+    def _split_meta_keys(cls, config: dict | DictConfig) -> tuple[dict, dict]:
+        """Splits the meta-keys from the rest of the configuration.
+
+        Args:
+            config: The configuration to split.
+
+        Returns:
+            A tuple containing the dictionary's meta-keys and the rest of the configuration.
+
+        Examples:
+            >>> StageConfig._split_meta_keys({"_base_stage": "base_stage", "param1": 1})
+            ({'_base_stage': 'base_stage'}, {'param1': 1})
+            >>> StageConfig._split_meta_keys({"param1": 1})
+            ({}, {'param1': 1})
+            >>> StageConfig._split_meta_keys({"_base_stage": "base_stage"})
+            ({'_base_stage': 'base_stage'}, {})
+        """
+        meta_keys = {}
+        non_meta_keys = {}
+
+        for k, v in config.items():
+            if StageConfig._is_meta_key(k):
+                meta_keys[k] = v
+            else:
+                non_meta_keys[k] = v
+
+        return meta_keys, non_meta_keys
+
+    @classmethod
+    def _dict_arg_error_str(cls, arg: dict | DictConfig) -> str | None:
+        """Checks if the argument is a valid dictionary representation of a stage configuration.
+
+        Args:
+            arg: The argument to check.
+
+        Returns:
+            True if the argument is a valid dictionary representation, False otherwise. A valid dictionary has
+                all keys as strings and has only one key that is not a meta-key, which points to a dictionary.
+
+        Examples:
+            >>> print(StageConfig._dict_arg_error_str({"stage": {"param1": 1}}))
+            None
+            >>> print(StageConfig._dict_arg_error_str({"stage": {"_base_stage": "base_stage", "param1": 1}}))
+            None
+            >>> print(StageConfig._dict_arg_error_str({"stage": {"foo": "foobar"}, "_base_stage": "base"}))
+            None
+            >>> print(StageConfig._dict_arg_error_str({123: {"param1": 1}}))
+            All keys must be strings. Got key(s):
+              - int: 123
+            >>> print(StageConfig._dict_arg_error_str({"stage": {"param1": 1}, "param2": 2}))
+            Expected a single non-meta key-value pair representing the stage name and configuration. Got 2
+            non-meta keys: stage, param2.
+            >>> print(StageConfig._dict_arg_error_str({"S": {"_base_stage": "foo"}, "_base_stage": "foo"}))
+            You cannot specify meta keys in both the raw and stage configuration dictionaries. Got duplicate
+            meta keys: _base_stage.
+        """
+        if any(not isinstance(k, str) for k in arg.keys()):
+            non_str_keys = [k for k in arg.keys() if not isinstance(k, str)]
+            err_strs = [f"  - {type(key).__name__}: {key}" for key in non_str_keys]
+            err_str = "\n".join(err_strs)
+            return f"All keys must be strings. Got key(s):\n{err_str}"
+
+        meta_keys, non_meta_keys = cls._split_meta_keys(arg)
+        if len(non_meta_keys) != 1:
+            return (
+                "Expected a single non-meta key-value pair representing the stage name and configuration. "
+                f"Got {len(non_meta_keys)} non-meta keys: {', '.join(non_meta_keys.keys())}."
+            )
+
+        stage_config = list(non_meta_keys.values())[0]
+
+        meta_key_duplicates = [k for k in meta_keys if k in stage_config]
+        if meta_key_duplicates:
+            return (
+                "You cannot specify meta keys in both the raw and stage configuration dictionaries. "
+                f"Got duplicate meta keys: {', '.join(meta_key_duplicates)}."
+            )
+        return None
+
+    @classmethod
+    def from_arg(cls, arg: str | dict | DictConfig) -> StageConfig:
+        """Resolves a `StageConfig` object from a given argument (yaml file) representation.
+
+        Args:
+            arg: The argument to resolve. This can be a string representing the stage name, indicating no
+                additional configuration arguments, a dictionary representing the stage configuration in
+                resolved form, or a dictionary with a single key-value, non-meta-key pair representing the
+                stage name and its configuration, with meta-keys (e.g., `_base_stage`) being pulled from the
+                base dict or the stage configuration as appropriate.
+
+        Returns:
+            A `StageConfig` object representing the resolved stage configuration.
+
+        Raises:
+            ValueError: If the argument is not a valid stage configuration.
+            TypeError: If the argument is not a string, dictionary, or `DictConfig`.
+
+        Examples:
+            >>> StageConfig.from_arg("stage_name")
+            StageConfig(name='stage_name', base_stage=None, config=None)
+            >>> StageConfig.from_arg({"stage_name": {"param1": 1}})
+            StageConfig(name='stage_name', base_stage=None, config={'param1': 1})
+            >>> StageConfig.from_arg({"stage_name": {"_base_stage": "base_stage", "param1": 1}})
+            StageConfig(name='stage_name', base_stage='base_stage', config={'param1': 1})
+            >>> StageConfig.from_arg({"foobar": {"param1": 1}, "_base_stage": "barfoo"})
+            StageConfig(name='foobar', base_stage='barfoo', config={'param1': 1})
+
+            Errors are thrown if the argument is the wrong type:
+
+            >>> StageConfig.from_arg(3)
+            Traceback (most recent call last):
+                ...
+            TypeError: Invalid argument type <class 'int'>. Expected str, dict, or DictConfig.
+
+            Or if the argument is not a valid stage configuration. For example, if the argument is not a
+            single (non-meta) key-value pair:
+
+            >>> StageConfig.from_arg({"stage_name": {"param1": 1}, "param2": 2})
+            Traceback (most recent call last):
+                ...
+            ValueError: Invalid stage config: Expected a single non-meta key-value pair representing the stage
+            name and configuration. Got 2 non-meta keys: stage_name, param2.
+
+            Or if the argument is not a valid stage name, or if meta-keys are used improperly:
+
+            >>> StageConfig.from_arg({123: {"param1": 1}})
+            Traceback (most recent call last):
+                ...
+            ValueError: Invalid stage config: All keys must be strings. Got key(s):
+              - int: 123
+            >>> StageConfig.from_arg({"stage_name": {"_base_stage": "foobar"}, "_base_stage": "barfoo"})
+            Traceback (most recent call last):
+                ...
+            ValueError: Invalid stage config: You cannot specify meta keys in both the raw and stage
+                configuration dictionaries. Got duplicate meta keys: _base_stage.
+        """
+
+        match arg:
+            case str() as name:
+                return cls(name=name)
+            case dict() | DictConfig() as config:
+                if cls._dict_arg_error_str(config):
+                    raise ValueError(f"Invalid stage config: {cls._dict_arg_error_str(config)}")
+
+                meta_keys, non_meta_keys = cls._split_meta_keys(config)
+                stage_name = list(non_meta_keys.keys())[0]
+                stage_config = non_meta_keys[stage_name]
+
+                meta_keys = {k[1:]: v for k, v in meta_keys.items()}
+                for k in cls.META_KEYS:
+                    if k in stage_config:
+                        meta_keys[k[1:]] = stage_config.pop(k)
+
+                return cls(name=stage_name, config=stage_config, **meta_keys)
+            case _:
+                raise TypeError(f"Invalid argument type {type(arg)}. Expected str, dict, or DictConfig.")
+
+
+@dataclasses.dataclass
 class PipelineConfig:
     """A base configuration class for MEDS-transforms pipelines.
 
@@ -79,11 +281,7 @@ class PipelineConfig:
     configuration object for a stage and pipeline realization from arguments.
 
     Attributes:
-        stages: A list of stage names in the pipeline. Stages will be executed in the order they are
-            specified.
-        stage_configs: A dictionary of stage configurations. Each key is a stage name, and the values are the
-            stage-specific arguments for that stage. Default values are provided in the stage's default
-            configuration object.
+        stages: A list of Stage configuration objects that are part of the pipeline.
         additional_params: A dictionary of additional parameters that are not stage-specific.
 
     Examples:
@@ -250,11 +448,11 @@ class PipelineConfig:
         return OmegaConf.create(merged)
 
     @classmethod
-    def from_arg(cls, pipeline_yaml: str | Path) -> PipelineConfig:
+    def from_arg(cls, arg: str | Path) -> PipelineConfig:
         """Construct a pipeline configuration object from a specified pipeline YAML file.
 
         Args:
-            pipeline_yaml: The path to the pipeline YAML file on disk or in the
+            arg: The path to the pipeline YAML file on disk or in the
                 'pkg://<pkg_name>.<relative_path>' format. It can also be the sentinel `__null__` string,
                 which will return an empty PipelineConfig object.
 
@@ -315,27 +513,25 @@ class PipelineConfig:
             FileNotFoundError: Pipeline YAML file '/tmp/tmp...' is a directory, not a file!
         """
 
-        match pipeline_yaml:
-            case str() if pipeline_yaml == NULL_STR:
+        match arg:
+            case str() if arg == NULL_STR:
                 return cls()
-            case str() as pkg_path if pipeline_yaml.startswith(PKG_PFX):
+            case str() as pkg_path if arg.startswith(PKG_PFX):
                 pipeline_fp = resolve_pkg_path(pkg_path)
             case str() | Path() as path:
                 pipeline_fp = Path(path)
             case _:
-                raise TypeError(
-                    f"Invalid pipeline YAML path type {type(pipeline_yaml)}. Expected str or Path."
-                )
+                raise TypeError(f"Invalid pipeline YAML path type {type(arg)}. Expected str or Path.")
 
         if pipeline_fp.exists() and pipeline_fp.is_dir():
-            raise FileNotFoundError(f"Pipeline YAML file '{pipeline_yaml}' is a directory, not a file!")
+            raise FileNotFoundError(f"Pipeline YAML file '{pipeline_fp}' is a directory, not a file!")
         elif pipeline_fp.suffix not in YAML_EXTENSIONS:
             raise ValueError(
                 f"Invalid pipeline YAML path '{pipeline_fp}'. "
                 f"Expected a file with one of the following extensions: {sorted(YAML_EXTENSIONS)}"
             )
         elif not pipeline_fp.exists():
-            raise FileNotFoundError(f"Pipeline YAML file '{pipeline_yaml}' does not exist.")
+            raise FileNotFoundError(f"Pipeline YAML file '{pipeline_fp}' does not exist.")
 
         as_dict_config = OmegaConf.load(pipeline_fp)
 
