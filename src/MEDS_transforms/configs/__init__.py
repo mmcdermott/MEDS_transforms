16d0506edd08544ab80e156096ce635889f55742
Matthew McDermott <mattmcdermott8@gmail.com>
2025-04-11 10:20:38 -0400

    Simplified ruff options

--- src/MEDS_transforms/configs/__init__.py
+++ src/MEDS_transforms/configs/__init__.py
@@ -2,10 +2,11 @@ from importlib.resources import files
 
 from .. import __package_name__
 
-RUNNER_CONFIG_YAML = files(__package_name__).joinpath("configs/_runner.yaml")
-
 from .dataset import DatasetConfig
 from .pipeline import PipelineConfig
 from .stage import StageConfig
+
+
+RUNNER_CONFIG_YAML = files(__package_name__).joinpath("configs/_runner.yaml")
 
 __all__ = ["DatasetConfig", "PipelineConfig", "StageConfig"]
